8708b1002d434d528b24e683355d699375ced05f
Matheus <matheusleandroferreira@gmail.com>
2025-07-15 11:21:00 -0300

    Classe para expor os endpoints da rinha

---
+++ paymentgateway/src/main/java/rinha_backend_2025/paymentgateway/controller/PaymentController.java
@@ -0,0 +1,35 @@
+package rinha_backend_2025.paymentgateway.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rinha_backend_2025.paymentgateway.dto.PaymentRequest;
+import rinha_backend_2025.paymentgateway.service.PaymentService;
+
+import java.time.ZonedDateTime;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/payments")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<Void> receive(@RequestBody PaymentRequest request) {
+        paymentService.enqueue(request);
+        return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping("/summary")
+    public ResponseEntity<Map<String, Map<String, Object>>> getSummary(
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime from,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime to
+    ) {
+        return ResponseEntity.ok(paymentService.getSummary(from, to));
+    }
+}
